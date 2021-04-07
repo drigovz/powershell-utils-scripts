ae9c8be1c62d12c3cff7d3649801f6d1720c6339
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-06 21:21:59 -0300

    feat: finished script to clean recycle bin

--- MyPsFunctions.psm1
+++ MyPsFunctions.psm1
@@ -1,9 +1,10 @@
 # clean recycle bin
-function My_EmptyRecycleBin {
+function MyPs_EmptyRecycleBin {
     $Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
     $Recycler.items() | ForEach-Object { Remove-Item $_.path -force -recurse }
 
     Write-Host "Recycle bin cleaned!!!"
 }
 
-Export-ModuleMember -Function My_EmptyRecycleBin
+# export functions
+Export-ModuleMember -Function MyPs_EmptyRecycleBin
