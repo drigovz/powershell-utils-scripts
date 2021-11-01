5ce2d5e3b21010b3516ef9c963100f537490839a
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-11-01 20:12:23 -0300

    feat: fix case sensitive for rename

--- find-and-replace/find_replace.ps1
+++ find-and-replace/find_replace.ps1
@@ -3,7 +3,7 @@ $oldValue = Read-Host "Hi $user, what is the word that you would like to replace
 $newValue = Read-Host "Replace to? ";
 
 # replace folders
-Get-ChildItem $currentLocation -R | Rename-Item -NewName { $_.Name -replace $oldValue, $newValue }
+Get-ChildItem $currentLocation -R | Rename-Item -NewName { $_.Name -creplace $oldValue, $newValue }
 
 $currentLocation = Get-Location;
 Get-ChildItem $currentLocation -Recurse;
@@ -14,7 +14,7 @@ foreach ($file in Get-ChildItem $currentLocation -Recurse) {
     if (-not  ($file.Extension -eq '.ps1')) {
         # if not is folder
         if (-not $file.PSIsContainer) {
-            ((Get-Content -Path $file.FullName -Raw) -replace $oldValue, $newValue) | Set-Content -Path $file.FullName
+            ((Get-Content -Path $file.FullName -Raw) -creplace $oldValue, $newValue) | Set-Content -Path $file.FullName
         }
     }
 }
